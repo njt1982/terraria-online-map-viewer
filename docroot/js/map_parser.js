703abb339904bcf9f5a6ede08ea927b3338086c2
Nicholas Thompson <njt1982@gmail.com>
2013-10-08 23:14:52 +0100

    Removing BitRatchet, using Binary AJAX by nihilogic

--- docroot/js/map_parser.js
+++ docroot/js/map_parser.js
@@ -11,55 +11,32 @@
     }
 
     var writeOut = function(data) {
-      var span = document.createElement('span');
-      span.innerHTML = data;
+      var el = document.createElement('li');
+      el.innerHTML = data;
       document.getElementById('output').insertBefore(span, null);
     };
     var writeInfoOut = function(label, data) {
       writeOut([
-        '<strong>' + label + '</strong> :',
+        '<strong>' + label + '</strong> : ',
         '<span>' + data + '</strong>'
       ].join(''));
     };
 
-    // var reader = new FileReader();
-    // reader.onloadend = function(e) {
-    //   console.log(e);
-    //   if (e.target.readyState == FileReader.DONE) {
-    //     writeInfoOut('Map Version', e.target.result);
-    //   }
-    // };
-
-    // var blob = map_file.slice(0, 4);
-    // reader.readAsBinaryString(blob);
-
-    // var reader = new FileReader();
-    // reader.onloadend = function(e) {
-    //   if (e.target.readyState == FileReader.DONE) {
-    //     console.log(e);
-    //     var r = new Int32Array(e.target.result, 0, 1);
-    //     console.log(r);
-    //   }
-    // };
-    // reader.readAsArrayBuffer(map_file);
-
-
     var reader = new FileReader();
     reader.onloadend = function(e) {
+      console.log(e);
       if (e.target.readyState == FileReader.DONE) {
-        console.log(e);
-        var header = bitratchet.record({
-          version: bitratchet.number({ length : 16 }),
-          world_name_length: bitratchet.number({ length : 8 }),
-          world_name: bitratchet.string({ length: 11 * 8 })
-        });
-        console.log(
-          header.parse(e.target.result).data
-        );
+        var f = new BinaryFile(e.target.result);
+
+        writeInfoOut('Map Version', f.getShortAt(0, 0));
+
+        var mapNameLength = f.getShortAt(8);
+        writeInfoOut('Map Name Length', mapNameLength);
+        writeInfoOut('Map Name', f.getStringAt(12, mapNameLength));
       }
     };
-    reader.readAsArrayBuffer(map_file);
 
+    reader.readAsBinaryString(map_file);
 
   }
 
